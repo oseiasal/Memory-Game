f5d13c5177f63c56e8035faf0c9f85c824c5f50e
Oseias <Oseiasanascimento@hotmail.com>
2018-06-12 14:58:58 -0300

    adicionei função ao botão repeat do modal

--- js/app.js
+++ js/app.js
@@ -1,152 +1,154 @@
-
-$( document ).ready(function(){
-let cards = ['fa-paper-plane-o', 'fa-anchor', 'fa-bicycle', 'fa-bomb',
+$(document).ready(function () {
+    let cards = ['fa-paper-plane-o', 'fa-anchor', 'fa-bicycle', 'fa-bomb',
 							'fa-leaf', 'fa-bolt', 'fa-cube', 'fa-diamond',
 							'fa-paper-plane-o', 'fa-anchor', 'fa-bicycle', 'fa-bomb',
 							'fa-leaf', 'fa-bolt', 'fa-cube', 'fa-diamond'];
 
-let cardOpen = [];
-let cardMatch = [];
-let value1, value2, fisrtCard, secondCard;
-let cardWaiting = false;
-let counter = 0;
+    let cardOpen = [];
+    let cardMatch = [];
+    let value1, value2, fisrtCard, secondCard;
+    let cardWaiting = false;
+    let counter = 0;
 
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-		let currentIndex = array.length,
-				temporaryValue, randomIndex;
+    // Shuffle function from http://stackoverflow.com/a/2450976
+    function shuffle(array) {
+        let currentIndex = array.length,
+            temporaryValue, randomIndex;
 
-		while (currentIndex !== 0) {
-				randomIndex = Math.floor(Math.random() * currentIndex);
-				currentIndex -= 1;
-				temporaryValue = array[currentIndex];
-				array[currentIndex] = array[randomIndex];
-				array[randomIndex] = temporaryValue;
-		}
+        while (currentIndex !== 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
 
-		return array;
-}
-//botão repeat
-$('.fa-repeat').click(function(){
-	$('.deck').children().removeClass('show match open');
-	$('.deck .fa').removeClass().addClass('fa');
-	$('#star1').removeClass('white');
-	$('#star2').removeClass('white');
-	$('#star3').removeClass('white');
-	counter = 0;
-	$('.moves').text(counter);
-		shuffleCards();
-});
+        return array;
+    }
+    //botão repeat
+		repeat();
+		function repeat(){
+    $('.fa-repeat').click(function () {
+        $('.deck').children().removeClass('show match open');
+        $('.deck .fa').removeClass().addClass('fa');
+        $('#star1').removeClass('white');
+        $('#star2').removeClass('white');
+        $('#star3').removeClass('white');
+				$('.modal').removeClass('modal-show');
+        counter = 0;
+        $('.moves').text(counter);
+        shuffleCards();
+    });}
 
-shuffleCards();
-function shuffleCards(){
-	cards = shuffle(cards);
-$('.deck .fa').each(function (index, elemento) {
-		$(this).addClass(cards[index]);
-});
-}
+    shuffleCards();
 
-$('.deck').on('click', '.card', handler); // listener Card OK
+    function shuffleCards() {
+        cards = shuffle(cards);
+        $('.deck .fa').each(function (index, elemento) {
+            $(this).addClass(cards[index]);
+        });
+    }
 
-function handler(event) {
+    $('.deck').on('click', '.card', handler); // listener Card OK
 
-		if ($(this).hasClass('match')) {
+    function handler(event) {
 
-				alert("Select another Card");
+        if ($(this).hasClass('match')) {
 
-		} else {
+            alert("Select another Card");
 
-				$(this).toggleClass('open show');
-				checkCardFlag();
+        } else {
 
-				if (cardWaiting == true) {
-						firstCard = $(this).children().attr('class');
+            $(this).toggleClass('open show');
+            checkCardFlag();
 
-				} else {
-						secondCard = $(this).children().attr('class');
-						count();
+            if (cardWaiting == true) {
+                firstCard = $(this).children().attr('class');
 
-				}
+            } else {
+                secondCard = $(this).children().attr('class');
+                count();
 
-				if (firstCard != null && secondCard != null && cardWaiting == false) {
-						checkCardValues();
+            }
 
-				}
+            if (firstCard != null && secondCard != null && cardWaiting == false) {
+                checkCardValues();
 
-		}
-		deadStar();
-		myModal();
-}
+            }
 
-function checkCardValues() {
-		value1 = firstCard;
-		value2 = secondCard;
+        }
+        deadStar();
+        myModal();
+    }
 
-		if (value1 == value2) {
-				value1 = 0; //reseta
-				value2 = 0; //reseta
-				firstCard = null; //reseta
-				secondCard = null; //reseta
-				$('.deck').find('.open').addClass('match').removeClass('open show');
-		} else {
-				setTimeout(nonMatch(), 300);
-				setTimeout(function () {
-						$('.deck').find('.wrong').removeClass('wrong')
-				}, 500);
-		}
-}
+    function checkCardValues() {
+        value1 = firstCard;
+        value2 = secondCard;
 
-function checkCardFlag() {
+        if (value1 == value2) {
+            value1 = 0; //reseta
+            value2 = 0; //reseta
+            firstCard = null; //reseta
+            secondCard = null; //reseta
+            $('.deck').find('.open').addClass('match').removeClass('open show');
+        } else {
+            setTimeout(nonMatch(), 300);
+            setTimeout(function () {
+                $('.deck').find('.wrong').removeClass('wrong')
+            }, 500);
+        }
+    }
 
-		if (cardWaiting == false) {
-				cardWaiting = true;
-		} else {
-				cardWaiting = false;
-		}
-		return cardWaiting;
-}
+    function checkCardFlag() {
 
-function nonMatch() {
-		$('.deck').find('.open').removeClass('open show').addClass('wrong');
-}
+        if (cardWaiting == false) {
+            cardWaiting = true;
+        } else {
+            cardWaiting = false;
+        }
+        return cardWaiting;
+    }
 
-function count() {
-	var moves = $('.moves');
-	counter = counter + 1;
-	moves.text(counter)
-}
+    function nonMatch() {
+        $('.deck').find('.open').removeClass('open show').addClass('wrong');
+    }
+
+    function count() {
+        var moves = $('.moves');
+        counter = counter + 1;
+        moves.text(counter)
+    }
 
 
-function deadStar(){
-	if (counter == 16) {
-	$('#star3').addClass('white');
-}
-else if (counter == 24) {
-	$('#star2').addClass('white');
-}
-else if (counter == 32) {
-	$('#star1').addClass('white');
-}
-}
+    function deadStar() {
+        if (counter == 16) {
+            $('#star3').addClass('white');
+        } else if (counter == 24) {
+            $('#star2').addClass('white');
+        } else if (counter == 32) {
+            $('#star1').addClass('white');
+        }
+    }
 
-function myModal(){
-	if ($('.deck').find('.match').length == 16){
-	$('.modal').toggleClass('modal-show');
-	var text = $('p').text();
-	$('p').text(text + " com " + counter + " movimentos");
-	}
-}
+    function myModal() {
+        if ($('.deck').find('.match').length == 16) {
+            $('.modal').toggleClass('modal-show');
+            var text = $('p').text();
+            $('p').text(text + " com " + counter + " movimentos");
+        }
+				repeat();
+    }
 
-console.log("DOM is ready");
-/*
- * set up the event listener for a card. If a card is clicked: OK
- *  - display the card's symbol (put this functionality in another function that you call from this one) OK
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one) OK
- *  - if the list already has another card, check to see if the two cards match ? OK
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one) OK
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one) OK
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+    console.log("DOM is ready");
+    /*
+     * set up the event listener for a card. If a card is clicked: OK
+     *  - display the card's symbol (put this functionality in another function that you call from this one) OK
+     *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one) OK
+     *  - if the list already has another card, check to see if the two cards match ? OK
+     *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one) OK
+     *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one) OK
+     *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+     *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+     */
 });
