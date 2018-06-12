90910b382f2eb07a683042692e174faae9e9a885
Oseias <Oseiasanascimento@hotmail.com>
2018-06-12 11:55:11 -0300

    removed trim spaces and add a modal function

--- js/app.js
+++ js/app.js
@@ -1,47 +1,47 @@
-let cards = ['fa-paper-plane-o', 'fa-anchor', 'fa-bicycle', 'fa-bomb', 'fa-leaf', 'fa-bolt', 'fa-cube', 'fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bicycle', 'fa-bomb', 'fa-leaf', 'fa-bolt', 'fa-cube', 'fa-diamond'];
+let cards = ['fa-paper-plane-o', 'fa-anchor', 'fa-bicycle', 'fa-bomb',
+							'fa-leaf', 'fa-bolt', 'fa-cube', 'fa-diamond',
+							'fa-paper-plane-o', 'fa-anchor', 'fa-bicycle', 'fa-bomb',
+							'fa-leaf', 'fa-bolt', 'fa-cube', 'fa-diamond'];
 
 let cardOpen = [];
 let cardMatch = [];
-let value1;
-let value2;
-let fisrtCard;
-let secondCard;
+let value1, value2, fisrtCard, secondCard;
 let cardWaiting = false;
 let counter = 0;
-let star1;
-let star2;
-let star3;
 
 $( document ).ready(function(){
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    let currentIndex = array.length,
-        temporaryValue, randomIndex;
+		let currentIndex = array.length,
+				temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+		while (currentIndex !== 0) {
+				randomIndex = Math.floor(Math.random() * currentIndex);
+				currentIndex -= 1;
+				temporaryValue = array[currentIndex];
+				array[currentIndex] = array[randomIndex];
+				array[randomIndex] = temporaryValue;
+		}
 
-    return array;
+		return array;
 }
 //botão repeat
 $('.fa-repeat').click(function(){
-  $('.deck').children().removeClass('show match open');
-  $('.deck .fa').removeClass().addClass('fa')
-  counter = 0;
-  $('.moves').text(counter);
-    shuffleCards()
+	$('.deck').children().removeClass('show match open');
+	$('.deck .fa').removeClass().addClass('fa');
+	$('#star1').removeClass('white');
+	$('#star2').removeClass('white');
+	$('#star3').removeClass('white');
+	counter = 0;
+	$('.moves').text(counter);
+		shuffleCards();
 });
 
 shuffleCards();
 function shuffleCards(){
-  cards = shuffle(cards);
+	cards = shuffle(cards);
 $('.deck .fa').each(function (index, elemento) {
-    $(this).addClass(cards[index]);
+		$(this).addClass(cards[index]);
 });
 }
 
@@ -49,83 +49,91 @@ $('.deck').on('click', '.card', handler); // listener Card OK
 
 function handler(event) {
 
-    if ($(this).hasClass('match')) {
+		if ($(this).hasClass('match')) {
 
-        alert("Select another Card");
+				alert("Select another Card");
 
-    } else {
+		} else {
 
-        $(this).toggleClass('open show');
-        checkCardFlag();
+				$(this).toggleClass('open show');
+				checkCardFlag();
 
-        if (cardWaiting == true) {
-            firstCard = $(this).children().attr('class');
+				if (cardWaiting == true) {
+						firstCard = $(this).children().attr('class');
 
-        } else {
-            secondCard = $(this).children().attr('class');
-            count();
+				} else {
+						secondCard = $(this).children().attr('class');
+						count();
 
-        }
+				}
 
-        if (firstCard != null && secondCard != null && cardWaiting == false) {
-            checkCardValues();
+				if (firstCard != null && secondCard != null && cardWaiting == false) {
+						checkCardValues();
 
-        }
+				}
 
-    }
-    deadStar();
+		}
+		deadStar();
+		myModal();
 }
 
 function checkCardValues() {
-    value1 = firstCard;
-    value2 = secondCard;
+		value1 = firstCard;
+		value2 = secondCard;
 
-    if (value1 == value2) {
-        value1 = 0; //reseta
-        value2 = 0; //reseta
-        firstCard = null; //reseta
-        secondCard = null; //reseta
-        $('.deck').find('.open').addClass('match').removeClass('open show');
-    } else {
-        setTimeout(nonMatch(), 300);
-        setTimeout(function () {
-            $('.deck').find('.wrong').removeClass('wrong')
-        }, 500);
-    }
+		if (value1 == value2) {
+				value1 = 0; //reseta
+				value2 = 0; //reseta
+				firstCard = null; //reseta
+				secondCard = null; //reseta
+				$('.deck').find('.open').addClass('match').removeClass('open show');
+		} else {
+				setTimeout(nonMatch(), 300);
+				setTimeout(function () {
+						$('.deck').find('.wrong').removeClass('wrong')
+				}, 500);
+		}
 }
 
 function checkCardFlag() {
 
-    if (cardWaiting == false) {
-        cardWaiting = true;
-    } else {
-        cardWaiting = false;
-    }
-    return cardWaiting;
+		if (cardWaiting == false) {
+				cardWaiting = true;
+		} else {
+				cardWaiting = false;
+		}
+		return cardWaiting;
 }
 
 function nonMatch() {
-    $('.deck').find('.open').removeClass('open show').addClass('wrong');
+		$('.deck').find('.open').removeClass('open show').addClass('wrong');
 }
 
 function count() {
-  var moves = $('.moves');
-  counter = counter + 1;
-  moves.text(counter)
+	var moves = $('.moves');
+	counter = counter + 1;
+	moves.text(counter)
 }
 
 
 function deadStar(){
-  if (counter == 16) {
-  $('#star3').addClass('white');
+	if (counter == 16) {
+	$('#star3').addClass('white');
 }
 else if (counter == 24) {
-  $('#star2').addClass('white');
+	$('#star2').addClass('white');
 }
 else if (counter == 32) {
-  $('#star1').addClass('white');
+	$('#star1').addClass('white');
 }
 }
+
+function myModal(){
+	if (counter == 3){
+	$('.modal').toggleClass('modal-show');
+}
+}
+
 console.log("DOM is ready");
 /*
  * set up the event listener for a card. If a card is clicked: OK
